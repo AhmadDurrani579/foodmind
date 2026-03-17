76d4f7cb7ba228db51615bad5c1adf89df14a655
AhmadDurrani579 <ahmaddurranitrg@gmail.com>
2026-03-17 16:01:05 +0000

    get User Info

--- app/router/users.py
+++ app/router/users.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, Depends, Request
 from app.core.dependencies import get_current_user
-from app.schemas.user import UserResponse
+from app.schemas.user import UserResponse, UserMeResponse
 from app.router.auth import build_user_response
 
 router = APIRouter()
 
-@router.get("/users/me", response_model=UserResponse)
-def get_me(
-    request: Request,
-    user = Depends(get_current_user)
-):
-    return build_user_response(user, request)
+@router.get("/users/me", response_model=UserMeResponse)
+def get_me(request: Request, user=Depends(get_current_user)):
+
+    user_data = build_user_response(user, request)
+
+    return {
+        "success": True,
+        "user": user_data
+    }
--- app/schemas/user.py
+++ app/schemas/user.py
@@ -51,3 +51,7 @@ class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
     user: UserResponse
+
+class UserMeResponse(BaseModel):
+    success: bool
+    user: UserResponse
